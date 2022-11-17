d05f58f34b7a699a0853ddafbee26af950d17ffd
Furio Scopinich <116543040+Mastercineggro@users.noreply.github.com>
2022-11-17 08:42:59 +0000

    eserciizo fatto

--- main.cpp
+++ main.cpp
@@ -2,5 +2,27 @@
 using namespace std;
 
 int main() {
+   int opzione;
+   float a,b,result;
+   cin >> a >> b >> opzione;
+
+   switch (opzione){
+      case 0 :
+      result = (a*b)/2;
+      break;
+
+      case 1: 
+      result = a*a;
+      break;
+
+      case 2:
+      result = a*b;
+      break;
+      default:
+      cout << "opzione non e valide";
+      break;
+
+   }
+   cout << result;
    return 0;
 }
